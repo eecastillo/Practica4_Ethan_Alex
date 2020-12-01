2be44c1be0fcb712d7981fdc8fd20a75dd246cde
Alejandro Gudiño <gudo0399@gmail.com>
2020-11-30 23:53:58 -0600

    Filtros agregados

---
+++ filter.h
@@ -0,0 +1,23 @@
+/*
+ * freertos_I2C.h
+ *
+ *  Created on: Oct 15, 2020
+ *      Author: eduar
+ */
+
+#ifndef FILTER_H_
+#define FILTER_H
+#include "arm_math.h"
+
+#define NUM_TAPS 51
+#define BLOCK_SIZE 1042
+#define TEST_LENGTH_SAMPLES 4*1024
+
+void init_LP();
+void init_BP();
+void init_HP();
+void init_buff(uint32_t *buffer);
+void apply_filter(uint8_t select);
+
+
+#endif /* FREERTOS_I2C_H_ */
